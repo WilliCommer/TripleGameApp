b55ea405a823426c0c90a399a2f682ad47e14de2
Willi Commer <willicommer@gmail.com>
2018-11-04 20:08:31 +0100

    Add files via upload

---
+++ build/precache-manifest.2f48aa4f7ae60fdd9f5a6a1e987aa224.js
@@ -0,0 +1,86 @@
+self.__precacheManifest = [
+  {
+    "revision": "174f5e285200dbdc1a52af5bfbecc967",
+    "url": "./static/media/castle.174f5e28.png"
+  },
+  {
+    "revision": "a5e163ff656875ed39cc",
+    "url": "./static/css/main.5874ef98.chunk.css"
+  },
+  {
+    "revision": "167f7be0ddab107c7e05b4358ec640f3",
+    "url": "./static/media/bigtreasure.167f7be0.png"
+  },
+  {
+    "revision": "8c56818eea26c291375c",
+    "url": "./static/js/1.8c56818e.chunk.js"
+  },
+  {
+    "revision": "4a686d48d5a089750c49",
+    "url": "./static/js/runtime~main.4a686d48.js"
+  },
+  {
+    "revision": "f70367f5e88d199c70fd117ec46967f0",
+    "url": "./static/media/grass.f70367f5.png"
+  },
+  {
+    "revision": "42f655158f9fa45e50f724007ef91cad",
+    "url": "./static/media/bush.42f65515.png"
+  },
+  {
+    "revision": "ba509583c3df8503e2c371bf38b0a9cf",
+    "url": "./static/media/tree.ba509583.png"
+  },
+  {
+    "revision": "ba5dcaed1612db0c1e77210ae5b97f09",
+    "url": "./static/media/house.ba5dcaed.png"
+  },
+  {
+    "revision": "13151ecae8248d10c54f3ed597b38240",
+    "url": "./static/media/mansion.13151eca.png"
+  },
+  {
+    "revision": "a5e163ff656875ed39cc",
+    "url": "./static/js/main.a5e163ff.chunk.js"
+  },
+  {
+    "revision": "b2152c4e091ad6816095aef6a7c5b0e1",
+    "url": "./static/media/floatingcastle.b2152c4e.png"
+  },
+  {
+    "revision": "d0e313516d3ac3700a235f73d9df5755",
+    "url": "./static/media/triplecastle.d0e31351.png"
+  },
+  {
+    "revision": "6e24c4ec4ac8c2cb493b399cc7829993",
+    "url": "./static/media/crystal.6e24c4ec.png"
+  },
+  {
+    "revision": "c9ccdb46999ad8e85a856cb6669e24a2",
+    "url": "./static/media/tomb.c9ccdb46.png"
+  },
+  {
+    "revision": "201e531071203d6fb44edeb9faad2397",
+    "url": "./static/media/ninja.201e5310.png"
+  },
+  {
+    "revision": "5f432d861bc5b345acdec002067889d0",
+    "url": "./static/media/church.5f432d86.png"
+  },
+  {
+    "revision": "ff3fc1cfbe21fa563f64cccf5730a867",
+    "url": "./static/media/cathedral.ff3fc1cf.png"
+  },
+  {
+    "revision": "94b27e04354487ad25dc6d76cf3bb215",
+    "url": "./static/media/treasure.94b27e04.png"
+  },
+  {
+    "revision": "8c56818eea26c291375c",
+    "url": "./static/css/1.66dc9b35.chunk.css"
+  },
+  {
+    "revision": "0f2d517ac565ce06afe3836437c49978",
+    "url": "./index.html"
+  }
+];
